cd1ae8adb84ddefea0c543a94bf66cd4321592d3
omah-hub <orjichioma422@yahoo.com>
2023-01-05 09:20:37 -0800

    my commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     argc = len(argv) - 1
-    for argc != 3:
+    argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operator = argv[2]
