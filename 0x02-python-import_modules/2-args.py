e26661da687aaa9ea5d8e20b7c5645b20ea9dde2
omah-hub <orjichioma422@yahoo.com>
2023-01-05 04:36:29 -0800

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv) - 1
-
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
+    from sys import argv
+argc = len(argv)
+if argc < 2:
+    print("{} arguments.".format(argc - 1))
+else:
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
     else:
-        print("{} arguments:".format(argc))
-
-        if argc > 0:
-            for i in range(argc):
-                print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(argc - 1))
+    for n in range(1, argc):
+        print("{}: {}".format(n, argv[n]))
