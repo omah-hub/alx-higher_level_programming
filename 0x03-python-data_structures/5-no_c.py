ec8fe7ace7ffb979c2e6ef72e14192d475c3e643
omah-hub <orjichioma422@yahoo.com>
2023-01-08 11:12:58 -0800

    my commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = []
     for letter in my_string:
-        if letter != 'c' and letter != 'c':
+        if letter != 'c' and letter != 'C':
             result.append(letter)
         return ''.join(result)
