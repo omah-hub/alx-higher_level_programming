a7a22630459fe1b762d221008ee5304e2b9ce669
omah-hub <orjichioma422@yahoo.com>
2023-02-14 12:49:37 -0800

    my commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,16 +25,16 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-     @property
-     def height(self):
-         """getter for the private instance attribute height"""
-         return self.__height
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return self.__height
 
-     @height.setter
-     def height(self, value):
-         """setter for the private instance attribute height"""
-         if type(value) is not int:
-             raise TypeError("height must be an integer")
-         if value < 0:
+    @height.setter
+    def height(self, value):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
              raise ValueError("height must be >= 0")
-         self.__height = value
+        self.__height = value
