1b2d236f07f6f73889d779c3b3cfbb22ffbebc92
omah-hub <orjichioma.422@yahoo.com>
2023-05-01 07:06:56 -0700

    task done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch me
-curl -sL 0.0.0.0:5000/catch_me -X "user_id=98" PUT -H "Origin: HolbertonSchool"
+curl -s -L -X PUT -d "user_id=98" PUT -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
