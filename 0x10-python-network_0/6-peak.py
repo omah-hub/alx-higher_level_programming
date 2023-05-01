8349aa200b85cbf74c5fd7383c9a8915b3e21084
omah-hub <orjichioma.422@yahoo.com>
2023-05-01 07:14:57 -0700

    task done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,5 +12,5 @@ def find_peak(list_of_integers):
     if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
        return li[m]
     if m != l - 1 and li[m + 1] > li[m]:
-       return find_peak(li[m + 1])
+       return find_peak(li[m + 1:])
     return find_peak(li[:m])
